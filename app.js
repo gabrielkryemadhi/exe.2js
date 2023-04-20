93aa278eaf22942617d9129fa18c2ca7b2d4f044
Gabriel <Kryemadhigabriel@gmail.com>
2023-04-20 13:17:22 +0200

    added

---
+++ app.js
@@ -0,0 +1,23 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const eventDate = new Date(`May 01 2023 00:05:00`);
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = eventDate - currentTime;
+    const d = Math.floor(diff/1000/60/60/24);
+    const h = Math.floor(diff/1000/60/60) % 24;
+    const m = Math.floor(diff/1000/60) % 60;
+    const s = Math.floor(diff/1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
+
