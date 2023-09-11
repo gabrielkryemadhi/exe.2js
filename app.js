d0e756b41aa623c0f0865975656f4dfb8d80c62a
Gabriel <117278733+gabrielkryemadhi@users.noreply.github.com>
2023-09-11 13:02:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
-const eventDate = new Date(`<september 24 2023 01:00:00`);
+const eventDate = new Date(`<september 25 2023 01:00:00`);
 
 function updateCountdown() {
     const currentTime = new Date();
